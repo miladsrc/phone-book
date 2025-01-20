f417e969052511becee01a7167f5ee31a583fa15
milad <baranimiald14@gmail.com>
2025-01-20 16:47:18 +0330

    feature: open api conf

---
+++ src/main/java/com/example/demo/config/OpenAPIConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Management API")
+                        .version("1.0")
+                        .description("Manage users in a PostgreSQL-backed application")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
